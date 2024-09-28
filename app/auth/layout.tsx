ed0311c5dde2879b90d856fab5c153d672cf499f
Ruan Portella <ruanmorales29@gmail.com>
2024-09-28 19:47:54 -0300

    Feat: Cria formulario de login

---
+++ app/auth/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LayoutAuth({
+  children
+} : {
+  children: React.ReactNode
+}) {
+  return (
+    <div className='h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-red-400 to-blue-800'>
+    {children}
+    </div>
+  )
+}
